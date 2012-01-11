2f94e276e2496079e9dacbc405d3823b2e4a164a
vimalfreelancer <vimalb83@gmail.com>
2012-01-10 21:44:56 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railscoders::Application.config.secret_token = '4528c5aafc531e8e539316d1702692cd1ef664cceaf2c355529ace5488a0ecd71a5044638bbf3a7c8a54a4f34ffab79d54c4912cff66ae0c11a31b18e6dc015a'
